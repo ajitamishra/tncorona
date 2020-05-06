521ae293edadf8b006eb10d90241bfca352f8979
BhaskarDutta2209 <bhaskardutta2209@gmail.com>
2020-05-07 01:50:00 +0530

    Wrote smart contract, modified file system and truffle.config file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const FixNix = artifacts.require("FixNix");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(FixNix, "0x260523e0F597f3ce69d363c406596250aedBf25B", "0xE9c35d97EEd9250238e4D6B0787911749E608a0e", "0x741F2A80892906206528Ba47d9e99C9C042bB423");
 };
---
+++ src/contracts/FixNix.sol
@@ -0,0 +1,68 @@
+pragma solidity 0.5.16;
+
+
+contract FixNix {
+    mapping(uint256 => string) public user_data;
+    mapping(uint256 => string) public user_file;
+    mapping(uint256 => string) public user_state;
+
+    address private investigator_address;
+    address private reviewer_address;
+    address private analyst_address;
+
+    constructor(address _investigator, address _reviewer, address _analyst)
+        public
+    {
+        investigator_address = _investigator;
+        reviewer_address = _reviewer;
+        analyst_address = _analyst;
+    }
+
+    modifier allowedAccess(address _address) {
+        require(
+            _address == investigator_address ||
+                _address == reviewer_address ||
+                _address == analyst_address
+        );
+        _;
+    }
+
+    function get_file_hash(uint256 _id)
+        public
+        view
+        allowedAccess(msg.sender)
+        returns (string memory)
+    {
+        return (user_file[_id]);
+    }
+
+    function get_data_hash(uint256 _id)
+        public
+        view
+        allowedAccess(msg.sender)
+        returns (string memory)
+    {
+        return (user_data[_id]);
+    }
+
+    function upload_data_hash(uint256 _id, string memory _hash)
+        public
+        allowedAccess(msg.sender)
+    {
+        user_data[_id] = _hash;
+    }
+
+    function upload_file_hash(uint256 _id, string memory _hash)
+        public
+        allowedAccess(msg.sender)
+    {
+        user_file[_id] = _hash;
+    }
+
+    function update_state(uint256 _id, string memory _state)
+        public
+        allowedAccess(msg.sender)
+    {
+        user_state[_id] = _state;
+    }
+}
