a1931f01ab61f25a20c51745cf4ab618bc9592d4
ajitamishra <ajita00054@gmail.com>
2020-05-06 17:42:49 +0530

    getting form data on ipfs using ipfs-mini

--- src/Components/Test1.js
+++ src/Components/Test1.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 function Test1() {
-  const ipfs = require("ipfs-http-client");
+  //   const ipfs = require("ipfs-http-client");
   const buffer = require("Buffer");
-  const IPFS = ipfs({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
+  //   const IPFS = ipfs({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
+  const IPFS = require("ipfs-mini");
+  const ipfs = new IPFS({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+  });
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -14,19 +20,30 @@ function Test1() {
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     console.log(typeof formData);
     var myBuffer = buffer.from(JSON.stringify(formData));
     console.log(typeof myBuffer);
-    IPFS.add(myBuffer, (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("IPFS Working");
-        console.log("IPFS Rsult", res[0].hash);
-      }
-    });
+    let response;
+    // await IPFS.add(myBuffer, (err, res) => {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log("IPFS Working");
+    //     console.log("IPFS Rsult", res[0].hash);
+    //     response = res[0].hash;
+    //   }
+    // });
+    // // const node = await ipfs.create();
+
+    // const data = await IPFS.cat(response);
+    // console.log(data);
+    response = await ipfs.add(myBuffer);
+    console.log(response);
+
+    const ipfsres = await ipfs.catJSON(response);
+    console.log(ipfsres);
   };
   return (
     <div>
